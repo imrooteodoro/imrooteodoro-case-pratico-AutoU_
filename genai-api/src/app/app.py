3523b8f71c7c1e7ec7ce829b8cd9a88f2b6a3501
imrooteodoro <adelsonteodoro67@gmail.com>
2025-01-29 21:43:36 -0300

    feat: create app.py

---
+++ genai-api/src/app/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from routes.sendmessage import send_to_bot
+import os
+
+
+app = Flask(__name__, static_folder='public')
+
+send_to_bot(app)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
